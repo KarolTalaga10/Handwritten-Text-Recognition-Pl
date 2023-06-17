cc6fc07f2861eea9cd2a95577e5b24d2a2eb7e19
jakubszczudlo <jakubszczudlo40@gmail.com>
2023-06-17 21:32:19 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Handwritten-Text-Recognition-Pl',
+    version='1.0',
+    packages=['SimpleHTR', 'WordDetectorNN'],
+    url='https://github.com/KarolTalaga10/Handwritten-Text-Recognition-Pl',
+    license='',
+    author='Jakub Szczudło, Karol Talaga, Michał Wełna',
+    author_email='',
+    description=''
+)
